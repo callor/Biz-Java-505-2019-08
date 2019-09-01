515b8460e6923d159803166d48367a08381db36e
callor <callor@callor.com>
2019-09-01 13:36:04 +0900

    한글문자코드

--- Java_03/src/com/biz/var/Char_String_02.java
+++ Java_03/src/com/biz/var/Char_String_02.java
@@ -8,8 +8,25 @@ public class Char_String_02 {
 		char c2 = '대';
 		String s3 = "한민국";
 		
-		System.out.println(c1 + c2 + s3);
+		System.out.println(c1 + 0);
+		
+		//한글 문자 Uni Code 보
+		System.out.println(c2 + 0);
+
+		// 문자 + 문자열 => 문자열로 변환 출력
+		System.out.println(c2 + s3);
 		System.out.println(c1 + s3);
+
+		
+		// 영문자 + 한글문자 + 문자열
+		// => (영문자 + 한글문자) : int 형 유니코드로 변환
+		// => (영문자 + 한글문자) + 문자
+		System.out.println(c1 + c2 + s3);
+		
+		// 영문자와 한글문자를 덧셈 연산하면
+		// 유니코드로 값이 나타 난
+		System.out.println(c1 + c2);
+		
 		
 	}
 }
