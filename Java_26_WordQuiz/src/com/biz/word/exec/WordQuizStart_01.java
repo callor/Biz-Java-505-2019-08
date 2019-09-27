2d456f84c4b8f03a681717c5069bb164327cda6d
callor <callor@callor.com>
2019-09-27 16:34:14 +0900

    2019-09-27

---
+++ Java_26_WordQuiz/src/com/biz/word/exec/WordQuizStart_01.java
@@ -0,0 +1,51 @@
+package com.biz.word.exec;
+
+import java.util.List;
+import java.util.Scanner;
+
+import com.biz.word.domain.WordVO;
+import com.biz.word.service.WordListMakeService;
+import com.biz.word.service.WordQuizService;
+import com.biz.word.service.WordQuizServiceV2;
+
+public class WordQuizStart_01 {
+
+	public static void main(String[] args) {
+	
+		String wordFileName = "src/com/biz/word/필수어휘.txt";
+		WordListMakeService wk = new WordListMakeService();
+		WordQuizService wq = new WordQuizServiceV2();
+	
+		Scanner scan = new Scanner(System.in);
+		
+		try {
+			wk.makeWordList(wordFileName);
+			List<WordVO> wordVOList = wk.getWordVOList();
+			wq.setWordVOList(wordVOList);
+			
+			while(true) {
+				
+				System.out.println("=========================");
+				System.out.println("행복나라 영어 정복 v1");
+				System.out.println("=========================");
+
+				wq.viewEngWord();
+				wq.quizExec();
+				
+				System.out.println("아무키나 누르세요...종료(-0) ");
+				String strKey = scan.nextLine();
+				if(strKey.equals("-0")) break;
+				
+			}
+			System.out.println("GAME OVER!!");
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
--- Java_26_WordQuiz/src/com/biz/word/service/WordQuizServiceV2.java
+++ Java_26_WordQuiz/src/com/biz/word/service/WordQuizServiceV2.java
@@ -13,6 +13,13 @@ public class WordQuizServiceV2 extends WordQuizServiceV1 {
 	// wordVO를 현재클래스의 모든 method가 볼수 있다.
 	// == 값을 읽을 수 있다 
 	WordVO wordVO;
+	int nCount ;
+	
+	public WordQuizServiceV2() {
+		
+		nCount = 0;
+		
+	}
 	
 	// V1에서 선언된 wordVOList를 사용하기 위해서 제거
 	// List<WordVO> wordVOList ;
@@ -27,15 +34,38 @@ public class WordQuizServiceV2 extends WordQuizServiceV1 {
 	// 알파벳을 낱개로 분해한후
 	// 무작위로 섞어서 console에 보이시오
 	public void viewEngWord() {
+
+		// 단어맞추기를 1번이라도 틀린적이 있으면
+		// nCount가 0 보다 클것이고
+		// 그러면 이미 생성되어 있는 wordVO를
+		// private viewEngWord(wordVO)에게  보내서
+		// 다시한번 뒤섞은 뒤 보여주기
+		if(nCount > 0) {
+			System.out.println();
+			this.viewEngWord(wordVO);
+			return ;
+		}
 		
+		// 새로운 단어로 시작이 되는 경우
+		//  	게임을 최초로 시작한 경우
+		//  	앞에서 단어를 맞추었을 경우
 		Random rnd = new Random();
 		int nSize = wordVOList.size();
 		int index = rnd.nextInt(nSize);
 		
 		wordVO = wordVOList.get(index);
-		System.out.println(wordVO.getEngWord());
 		
-		String word = wordVO.getEngWord();
+		this.viewEngWord(wordVO);
+
+	} // end viewEngWord()
+	
+	private void viewEngWord(WordVO wVO) {
+
+		// 임시코드 실제 삭제될 코드
+		// System.out.println(wVO.getEngWord());
+		
+		String word = wVO.getEngWord();
+		
 		String[] words = word.split(""); // 알파벳단위로 분해하기
 		
 		// 문자열 배열을 문자열형 List로 바꾸는 방법
@@ -50,7 +80,7 @@ public class WordQuizServiceV2 extends WordQuizServiceV1 {
 		// ABS 순으로 정렬하는 방법
 		Collections.sort(quizWords);
 		System.out.println(quizWords);
-
+	
 	}
 
 	/*
@@ -67,16 +97,44 @@ public class WordQuizServiceV2 extends WordQuizServiceV1 {
 	public void quizExec() {
 		
 		Scanner scan = new Scanner(System.in);
-		System.out.print("단어입력 >> ");
+		System.out.println("----------------------");
+		System.out.print("단어입력 힌트(H) >> ");
 		String strEngWord = scan.nextLine();
-		if(wordVO.getEngWord().equalsIgnoreCase(strEngWord)) {
-			System.out.println("맞아었요");
-		} else {
-			System.out.println("틀렸어요");
-			System.out.println(wordVO.getEngWord());
-		}
-		System.out.println(wordVO.getKorMeans());
 		
+		if(strEngWord.equals("H")) {
+			System.out.println("-------------------------");
+			System.out.println(wordVO.getKorMeans());
+			
+			int half = wordVO.getEngWord().length() / 2;
+			System.out.println(wordVO.getEngWord().substring(0,half));
+			System.out.println("-------------------------");
+		
+		}
+		
+		
+		if(wordVO.getEngWord().equalsIgnoreCase(strEngWord)) {
+			System.out.println("--------------------");
+			System.out.println("맞았었요");
+			System.out.println("시도횟수 : " + (nCount + 1));
+			System.out.println("---------------------");
+			System.out.println(wordVO.getKorMeans());
+			System.out.println("---------------------");
+			nCount = 0;
+		} else {
+			System.out.println("--------------------");
+			System.out.println((nCount + 1) + " 번 틀렸어요");
+			System.out.println("--------------------");
+			/*
+			System.out.print("계속(Enter), 힌트보기(H+Enter)>>");
+			String strH = scan.nextLine();
+			if(strH.equals("H")) {
+				System.out.println("----------------------");
+				System.out.println(wordVO.getKorMeans());
+				System.out.println("----------------------");
+			}
+			*/
+			nCount ++;
+		}
 	}
 	
 	
