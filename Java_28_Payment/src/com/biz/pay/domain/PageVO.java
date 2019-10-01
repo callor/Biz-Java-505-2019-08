61093174fa57509a1a30d46b83d68e4b739e0a57
callor <callor@callor.com>
2019-10-01 13:15:40 +0900

    pay

---
+++ Java_28_Payment/src/com/biz/pay/domain/PageVO.java
@@ -0,0 +1,43 @@
+package com.biz.pay.domain;
+
+public class PageVO {
+	
+	private String paper;
+	private int count;
+	
+	/*
+	 * 기본생성자
+	 */
+	public PageVO() {
+	}
+
+	/*
+	 * 필드 생성자
+	 * 필드 변수를 주입받아서 값을 설정한다.
+	 */
+	public PageVO(String paper, int count) {
+		this.paper = paper;
+		this.count = count;
+	}
+
+	public String getPaper() {
+		return paper;
+	}
+	public void setPaper(String paper) {
+		this.paper = paper;
+	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	@Override
+	public String toString() {
+		return "PageVO [paper=" + paper + ", count=" + count + "]";
+	}
+	
+	
+	
+
+}
