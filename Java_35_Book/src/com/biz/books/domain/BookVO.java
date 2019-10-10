c5461dc30ed6ade244a3f6d0e198e8d331901b6f
callor <callor@callor.com>
2019-10-10 13:02:37 +0900

    참고자료

---
+++ Java_35_Book/src/com/biz/books/domain/BookVO.java
@@ -0,0 +1,15 @@
+package com.biz.books.domain;
+/*
+ * 도서정보를 관리하기 위한
+ * 도서정보를 저장, 연산하기 위한 domain class 선언
+ * 
+ * ISBN : b_isbn String
+ * 도서명 : b_title String
+ * 저자 : b_writer String
+ * 역자 : b_trans String
+ * 가격 : b_price init
+ * 발행일 : b_date String
+ */
+public class BookVO {
+
+}
