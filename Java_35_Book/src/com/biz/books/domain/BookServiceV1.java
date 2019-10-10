19dd8fb565adc85a528936231b9f47abfc34b79f
callor <callor@callor.com>
2019-10-10 13:35:14 +0900

    참고자료

---
+++ Java_35_Book/src/com/biz/books/domain/BookServiceV1.java
@@ -0,0 +1,20 @@
+package com.biz.books.domain;
+
+import java.util.List;
+
+/*
+ * 
+ * 도서정보.txt 파일을 읽어서
+ * 문자열을 콜론(:)을 기준으로 분리하고
+ * BookVO에 담은 후 bookList에 추가하는 method를 작성
+ * 
+ */
+public class BookServiceV1 {
+	
+	List<BookVO> bookList ;
+	
+	public void readBookInfo(String bookFile) {
+		
+	}
+
+}
--- Java_35_Book/src/com/biz/books/domain/BookVO.java
+++ Java_35_Book/src/com/biz/books/domain/BookVO.java
@@ -6,10 +6,20 @@ package com.biz.books.domain;
  * ISBN : b_isbn String
  * 도서명 : b_title String
  * 저자 : b_writer String
+ * 출판사 : b_comp String
  * 역자 : b_trans String
- * 가격 : b_price init
+ * 가격 : b_price int
+ * 페이지수 : b_page int
  * 발행일 : b_date String
  */
 public class BookVO {
+	
+	private String b_isbn;
+	private String b_title;
+	private String b_writer;
+	private String b_trans;
+	private int b_price;
+	private int b_page;
+	private String b_date;
 
 }
